e42639a470f574d0e6384a838932e0c30e742ca1
ASHMEDDF <ashmeddiaz5@gmail.com>
2024-11-09 11:34:52 -0500

    Add annotation Transactional.

--- src/main/java/com/btg/funds/service/FundService.java
+++ src/main/java/com/btg/funds/service/FundService.java
@@ -9,6 +9,7 @@ import com.btg.funds.repository.FundRepository;
 import com.btg.funds.repository.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -16,6 +17,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Service
+@Transactional
 public class FundService {
 
     private static final double INITIAL_BALANCE = 500_000;
